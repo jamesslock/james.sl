99d4092ec1a0878665540a4b31bfa864a907c1c5
James Seymour-Lock <james@simpleasmilk.co.uk>
2016-07-23 22:24:37 +0100

    Update home links

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import { Link } from 'react-router';
 
 // Import Atom Components
 import Section    from '../../atoms/Section/Section.js';
@@ -33,7 +32,7 @@ export default class Home extends Component {
               </AnimateSlideRight>
               <AnimateFadeIn>
                 <p className={s.heroIntro}>
-                  A genius, billionaire, playboy, philanthropist & pathological liar. Managing director of <Link to="http://simpleasmilk.com">Simple as Milk</Link> and user experience engineer at <Link to="https://unroll.me">Unroll.Me</Link>.
+                  A genius, billionaire, playboy, philanthropist & pathological liar. Managing director of <a href="http://simpleasmilk.com" target="_blank" rel="noopener">Simple as Milk</a> and user experience engineer at <a href="https://unroll.me" target="_blank" rel="noopener">Unroll.Me</a>.
                 </p>
               </AnimateFadeIn>
             </Container>
