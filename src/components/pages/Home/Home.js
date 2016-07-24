752851ec6ac63eb78139ea2ba4851f3d1dbe8c53
James Seymour-Lock <james@simpleasmilk.co.uk>
2016-07-24 18:27:22 +0100

    Add meta title

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Helmet from "react-helmet";
 
 // Import Atom Components
 import Section    from '../../atoms/Section/Section.js';
@@ -23,11 +24,21 @@ export default class Home extends Component {
   render() {
     return(
       <div>
+        <Helmet
+          title="James Seymour-Lock"
+          meta={[
+						{"name": "referrer", "content": "origin"},
+						{"name": "description", "content": "Managing director of Simple as Milk and user experience engineer at Unroll.Me."}
+          ]}
+          link={[
+						{"rel": "canonical", "href": "http://james.sl"}
+					]}
+        />
         <Hero
           size="full"
           title={["James ", <br/>, "Seymour-Lock"]}
         >
-          <p>A genius, billionaire, playboy, philanthropist & pathological liar. Managing director of <a href="http://simpleasmilk.com" target="_blank" rel="noopener">Simple as Milk</a> and user experience engineer at <a href="https://unroll.me" target="_blank" rel="noopener">Unroll.Me</a>.</p>
+          <p>Managing director of <a href="http://simpleasmilk.com" target="_blank" rel="noopener">Simple as Milk</a> and user experience engineer at <a href="https://unroll.me" target="_blank" rel="noopener">Unroll.Me</a>.</p>
         </Hero>
       </div>
     )
